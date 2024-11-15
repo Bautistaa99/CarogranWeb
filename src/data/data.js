e64ef169a7a203c91f777f2dc8eddd18bcc12234
Bautistaa99 <jbautistavarela@gmail.com>
2024-11-15 14:58:27 -0300

    Carogran3.2(antes del carrusel)

--- src/data/data.js
+++ src/data/data.js
@@ -2,27 +2,27 @@ export const products = [
   {
     id: "1",
     nombre: "Arroz Largo Fino",
-    calidad: "00000",
-    alias: "Arroz 00000",
-    descripcion:
-      "Sumérgete en la excelencia culinaria con nuestro arroz Largo Fino 00000. Reconocido por su grano extra largo, calidad premium y elección excepcional, este arroz excepcionalmente refinado ofrece una experiencia culinaria incomparable. Cada grano, cuidadosamente seleccionado, garantiza una textura delicada y un sabor incomparable. Con hasta un 15% de granos partidos, este arroz conserva su integridad y calidad, asegurando resultados perfectos en cada plato. Descubre el verdadero placer de cocinar con nuestro arroz Largo Fino 00000 y eleva tus comidas a nuevas alturas de exquisitez.",
-    src: "000001kg.webp",
-    alt: "Arroz largo fino 00000",
-    codigo: "LargoFino00000",
-    src2:"IF00000.webp"
-  },
-  {
-    id: "2",
-    nombre: "Arroz Largo Fino",
     calidad: "0000",
     alias: "Arroz 0000",
     descripcion:
       "Sumérgete en la esencia de la calidad suprema con nuestro arroz Largo Fino 0000. Caracterizado por su grano alargado y su textura delicada, este arroz premium ofrece una experiencia culinaria incomparable. Con hasta un 25% de granos partidos cuidadosamente seleccionados, cada bocado ofrece una fusión perfecta de sabor y consistencia. Disfruta de la excelencia en cada plato con nuestro arroz Largo Fino 0000, la elección ideal para los paladares más exigentes.",
-    src: "00001kg.webp",
+    src: "0000x1kg.svg",
     alt: "Arroz largo fino 0000",
     codigo: "LargoFino0000",
     src2:"IF0000.webp"
 
+  },
+  {
+    id: "2",
+    nombre: "Arroz Largo Fino",
+    calidad: "00000",
+    alias: "Arroz 00000",
+    descripcion:
+      "Sumérgete en la excelencia culinaria con nuestro arroz Largo Fino 00000. Reconocido por su grano extra largo, calidad premium y elección excepcional, este arroz excepcionalmente refinado ofrece una experiencia culinaria incomparable. Cada grano, cuidadosamente seleccionado, garantiza una textura delicada y un sabor incomparable. Con hasta un 15% de granos partidos, este arroz conserva su integridad y calidad, asegurando resultados perfectos en cada plato. Descubre el verdadero placer de cocinar con nuestro arroz Largo Fino 00000 y eleva tus comidas a nuevas alturas de exquisitez.",
+    src: "000001kg.svg",
+    alt: "Arroz largo fino 00000",
+    codigo: "LargoFino00000",
+    src2:"IF00000.webp"
   },
   {
     id: "3",
@@ -30,7 +30,7 @@ export const products = [
     calidad: "Parboil “no se pasa”",
     descripcion:
       "El Arroz Parboil 00000 es la elección perfecta para aquellos que buscan una cocción precisa y resultados consistentes en sus platillos. Con su proceso de parboilizado, este arroz ofrece una textura suculenta y un sabor inigualable que complementa una amplia variedad de recetas. Desde acompañamientos clásicos hasta platos principales creativos, el arroz Parboil 00000 garantiza una experiencia culinaria excepcional en cada bocado. Descubre la versatilidad y la calidad premium de nuestro arroz Parboil 00000 y sorprende a tus comensales con platillos deliciosamente consistentes y suculentos en cada ocasión.",
-    src: "PARBOIL1KG.webp",
+    src: "Parboilx1kg.svg",
     alt: "Arroz parboil “no se pasa”",
     codigo: "Parboil",
     src2:"IFPARBOIL.webp"
@@ -41,7 +41,7 @@ export const products = [
     calidad: "“Doble Carolina”",
     descripcion:
       "Arroz Doble Carolina 00000, la elección perfecta para los paladares más exigentes. Este arroz de calidad distinguida se distingue por su grano extra largo y su textura tierna, lo que lo hace ideal para una amplia variedad de platos, desde deliciosos risottos hasta postres irresistibles. Con un proceso de selección meticuloso, garantizamos que cada grano esté en perfectas condiciones, ofreciendo una experiencia culinaria excepcional en cada bocado. Descubre la calidad premium de nuestro arroz Doble Carolina 00000 y eleva tus recetas a un nivel superior.",
-    src: "Doble1kg.webp",
+    src: "Doblex1kg.svg",
     alt: "Arroz doble carolina",
     codigo: "DobleCarolina",
     src2:"IFDOBLE.webp"
@@ -52,7 +52,7 @@ export const products = [
     calidad: "00000",
     descripcion:
       "El Arroz Integral 00000 es mucho más que un simple acompañamiento; es un alimento nutritivo y saludable que aporta una textura única y un sabor delicioso a tus comidas equilibradas. Con su grano entero y sin refinar, este arroz conserva todos sus nutrientes naturales, incluidas las vitaminas, minerales y fibra esencial. Disfruta de la versatilidad del Arroz Integral 00000 en una amplia variedad de platos, desde ensaladas frescas hasta guarniciones sabrosas y platos principales satisfactorios. Añade un toque de salud y sabor a tus comidas con el Arroz Integral 00000 y descubre el placer de una alimentación equilibrada y nutritiva.",
-    src: "Integral1kg.webp",
+    src: "Integralx1kg.svg",
     alt: "Arroz integral",
     codigo: "Integral",
     src2:"IFINTEGRAL.webp"
